77e3b08e02468d02e30950726557ccf4a5a3a5f2
avneet-kaur <avneepannu@gmail.com>
2021-03-29 19:23:34 +0530

    retrieving documents of movies from MongoDB

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 # server program
-
+from bson.json_util import dumps
 import datalink
+import DatasetUpload
 
 
 
@@ -15,6 +16,23 @@ def serverSetUp():
 
 def searchHandler(p_input_string):
     None
+    
+
+def retrievingDetailsOfMovies(movie_title_list):
+    """
+    read the list of movies and return details of each movie as a document from MongoDb
+    """
+    mongodb = DatasetUpload.MongoDB(dBName='MoviesRating', collectionName='movies_metadata')
+    for movie in movie_title_list:
+        movie_doc = mongodb.collection.find({"title": movie})
+        print("Retrieving document for movie: " + movie)
+        for document in movie_doc:
+            print(dumps(document))
+
+
+if __name__ == "__main__":
+    title_list = ['Toy Story', 'Jumanji', 'Grumpier Old Men', 'Father of the Bride Part II']
+    retrievingDetailsOfMovies(title_list)
 
 
 
