f1f65db0b0198c64ce9825102f7c41416f69a1e6
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-04-17 20:11:25 +0530

    Add files via upload

--- frontend/api.py
+++ frontend/api.py
@@ -1,13 +1,19 @@
-from flask import Flask, jsonify, request, render_template, flash
+from flask import Flask, request, render_template, flash
 import client
 
 app = Flask(__name__)
 
 app.secret_key = "key"
 
-db, db1 = client.connect_with_mongoDb()
 ClientStub = client.conncet_with_server()
 
+def fetch_db():
+    conn = client.connect_with_mongoDb()
+    db = conn.get_database('Clients')
+    db1 = conn.get_database('MovieRating')
+    return db , db1
+
+db,db1 = fetch_db()
 
 @app.route("/")
 def login():
@@ -44,21 +50,33 @@ def authentication():
     if request.method == "POST":
         username = request.form["Username"]
         password = request.form["Password"]
-        if (username and password and db):
-            queryObject = {'Name': username, 'Password': password}
-            query = db.users.find_one(queryObject)
-            if (query):
+        if (username and password):
+            l_count=0;
+            while(l_count<5):
+                print("***************************************************")
+                print("Wait... Verifying Your Credentials")
+                print("***************************************************")
                 try:
-                    print(query)
-                except:
-                    print("Error")
+                    l_count+=1;
+                    Response = ClientStub.atlas_instance(username,password)
+                    if (Response == True):                        
+                        print("***************************************************")
+                        print("Successfully Loged In")
+                        print("***************************************************")
+        
+                    else:                                               
+                        print("***************************************************")
+                        print("Error Occurred Please Try Again")
+                        print("***************************************************")
+                        flash("Incorrect Credentials")
+                        return render_template("login.html")
+                    
                 finally:
-                    return render_template("Home.html")
-
-            else:
-                flash("Incorrect Credentials")
-                return render_template("login.html")
+                    return render_template("Home.html")  
         else:
+            print("*******************************************************")
+            print("Empty Credentials")
+            print("*******************************************************")
             flash("Empty Credentials")
             return render_template("login.html")
 
@@ -69,27 +87,40 @@ def fetch():
         name = request.form["MovieName"]
 
         if name:
-
-            try:
-                print(ClientStub)
-                Movie = ClientStub.getrecommend(name)
-                print(Movie)
-                json_movie = []
-                print("Fetching")
-
-                for movie in Movie:
-                    queryObject = {"title": movie}
-                    query = db1.movies_metadata.find_one(queryObject, {"revenue": 1, "runtime": 1})
-                    query.pop('_id')
-                    json_movie.append(query)
-                    print(json_movie)
-
-                return json_movie
-
-            except:
-                return render_template("Home.html")
-            finally:
-                return render_template("Result.html", data=json_movie)
+            l_count = 0;
+            while(l_count<5):
+                print("*******************************************************")
+                print("Trying to connect to the Server....")
+                print("*******************************************************")
+                try:
+                    print("I m In")
+                    l_count+=1                    
+                    print(ClientStub)
+                    Movie = ClientStub.getrecommend(name)
+                    print(Movie)
+                    json_movie = []
+                    print("*******************************************************")
+                    print("Fetching")
+                    print("*******************************************************")
+    
+                    for movie in Movie:
+                        queryObject = {"title": movie}
+                        query = db1.movies_metadata.find_one(queryObject)
+                        query.pop('_id')
+                        json_movie.append(query)
+    
+                    return json_movie
+    
+                except:
+                    return render_template("Home.html")
+                finally:
+                    return render_template("Result.html", data=json_movie[0],data1=json_movie[1],data2=json_movie[2],data3=json_movie[3],data4=json_movie[4],data5=json_movie[5],data6=json_movie[6],data7=json_movie[7],data8=json_movie[8],data9=json_movie[9])
+        else:
+            print("*******************************************************")
+            print("Movie Name Not Entered By User")
+            print("*******************************************************")
+            flash("Please Enter The Movie Name")
+            return render_template("Machine Learning.html")
 
 
 @app.route("/Register", methods=['GET', 'POST'])
@@ -99,23 +130,35 @@ def register():
         password = request.form["Password"]
         ID = request.form["ID"]
         if username and password and ID:
-            try:
-                db = client.Clients
-                user = {
-                    "id": ID,
-                    "Name": username,
-                    "Password": password
-                }
-
-                db.users.insert_one(user)
-            except:
-                flash("Something Went Wrong")
-                return render_template("SignUp.html")
-            finally:
-                flash("Successfully Registered")
-                return render_template("login.html")
+            l_count=0
+            while(True):
+                print("*************************************************************************")
+                print("Trying To Register With The Server. Please Wait........................")
+                print("*************************************************************************")
+                try:
+                    l_count+=1;
+                    Response = ClientStub.register(username,password,ID)
+                    if(Response == True):
+                        print("*******************************************************")
+                        print(username + " You Got Successfully Registered")
+                        print("*******************************************************")
+                        
+                except:
+                    print("*******************************************************")
+                    print("Something Went Wrong")
+                    print("*******************************************************")
+                    return render_template("SignUp.html")
+                finally:
+                    print("*******************************************************")
+                    print(username + " You Got Successfully Registered")
+                    print("*******************************************************")
+                    flash("Successfully Registered")
+                    return render_template("login.html")
         else:
-            flash("Something Went Wrong")
+            print("*******************************************************")
+            print("Please Enter In Every Field")
+            print("*******************************************************")
+            flash("Please Enter In Every Field")
             return render_template("SignUp.html")
 
 
--- frontend/client.py
+++ frontend/client.py
@@ -7,12 +7,10 @@ def connect_with_mongoDb():
     try:
         connection_url = 'mongodb+srv://Manjit:Manjit@cluster0.dfnnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
         client = pymongo.MongoClient(connection_url)  
-        db = client.get_database('Clients')
-        db1 = client.get_database('MovieRating')
     except:
         print("Connection Failed With MongoDb Cloud")
     finally:
-        return db,db1
+        return client
     
 
 def conncet_with_server():
